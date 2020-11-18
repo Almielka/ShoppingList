a97e53bcf8458a00eeb6cfdb110696372eceb38d
Almielka <anna.s.almielka@gmail.com>
2020-11-18 17:37:57 +0100

    Migrating to Java15 and  Junit5

--- src/test/java/almielka/shoppinglist/controller/TaskControllerTest.java
+++ src/test/java/almielka/shoppinglist/controller/TaskControllerTest.java
@@ -5,14 +5,14 @@ import almielka.shoppinglist.domain.Category;
 import almielka.shoppinglist.domain.Task;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
@@ -42,7 +42,7 @@ public class TaskControllerTest {
 
     private static final String URL = "/tasks";
 
-    @Before
+    @BeforeEach
     public void init() {
         mockMvc = MockMvcBuilders.standaloneSetup(taskController).build();
     }
